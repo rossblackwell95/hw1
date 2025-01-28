1ac22ebf0bfb50e12eeb93e650e39b2512436de5
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-01-28 03:36:29 +0000

    Insert data into database

--- kmdb.sql
+++ kmdb.sql
@@ -144,7 +144,61 @@ CREATE TABLE roles (
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+INSERT INTO studios (
+  name
+)
+VALUES
+  ('Warner Bros.');
+
+INSERT INTO movies (
+  title,
+  year_released,
+  mpaa_rating,
+  studio_id
+)
+VALUES
+  ('Batman Begins', 2005, 'PG-13', 1),
+  ('The Dark Knight', 2008, 'PG-13', 1),
+  ('The Dark Knight Rises', 2012, 'PG-13', 1);
+
+INSERT INTO actors (
+  name
+)
+VALUES
+  ('Christian Bale'),
+  ('Michael Caine'),
+  ('Liam Neeson'),
+  ('Katie Holmes'),
+  ('Gary Oldman'),
+  ('Heath Ledger'),
+  ('Aaron Eckhart'),
+  ('Maggie Gyllenhaal'),
+  ('Tom Hardy'),
+  ('Joseph Gordon-Levitt'),
+  ('Anne Hathaway');
+
+INSERT INTO roles (
+  character_name,
+  movie_id,
+  actor_id
+)
+VALUES
+  ('Bruce Wayne', 1, 1),
+  ('Alfred', 1, 2),
+  ('Ra''s Al Ghul', 1, 3),
+  ('Rachel Dawes', 1, 4),
+  ('Commissioner Gordon', 1, 5),
+  ('Bruce Wayne', 2, 1),
+  ('Joker', 2, 6),
+  ('Harvey Dent', 2, 7),
+  ('Alfred', 2, 2),
+  ('Rachel Dawes', 2, 8),
+  ('Bruce Wayne', 3, 1),
+  ('Commissioner Gordon', 3, 5),
+  ('Bane', 3, 9),
+  ('John Blake', 3, 10),
+  ('Selina Kyle', 3, 11);
 
 -- Prints a header for the movies output
 .print "Movies"
